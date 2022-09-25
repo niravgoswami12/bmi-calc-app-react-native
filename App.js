75f10653638314087f65a6a9321615033e218d14
Nirav Goswami <niravgoswami12@gmail.com>
2022-09-24 21:02:44 -0400

    BMI calculated code added

--- App.js
+++ App.js
@@ -1,117 +1,182 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState } from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
+  Switch,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBmi] = useState('');
+  const [bmiLabel, setBmiLabel] = useState('');
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setHeight('');
+    setWeight('');
+    setBmi('');
+    setBmiLabel('');
+    setIsEnabled(previousState => !previousState);
+  };
+  handleHeight = e => {
+    setHeight(e);
+  };
+  handleWeight = e => {
+    setWeight(e);
+  };
+  calculate = (height, weight) => {
+    // Validare height/weight value, must be number
+    if (!height.match(/^\d+$/) || !weight.match(/^\d+$/)) {
+      alert('Please enter valid number!');
+      setBmiLabel('');
+      setBmi('');
+      return;
+    }
+    var result;
+    // Measurement system Imperial [cm, kg]
+    if (isEnabled) {
+      result =
+        (parseFloat(weight) * 10000) /
+        (parseFloat(height) * parseFloat(height));
+    } else {
+      // Measurement system Metric[in, lb]
+      result =
+        (parseFloat(weight) / (parseFloat(height) * parseFloat(height))) * 703;
+    }
+    result = result.toFixed(2);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // Display result as per BMI Categories:
+    // Underweight = <18.5
+    // Normal weight = 18.5–24.9
+    // Overweight = 25–29.9
+    // Obesity = BMI of 30 or greater
+    setBmi(result);
+    if (result < 18.5) {
+      setBmiLabel('Underweight');
+    } else if (result >= 18.5 && result < 25) {
+      setBmiLabel('Normal weight');
+    } else if (result >= 25 && result < 30) {
+      setBmiLabel('Overweight');
+    } else if (result >= 30) {
+      setBmiLabel('Obese');
+    } else {
+      alert('Incorrect Input!');
+      setBmiLabel('');
+      setBmi('');
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>BMI Calculator</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity style={isEnabled ? styles.toggleButtonActive : styles.toggleButton} onPress={toggleSwitch}>
+            <Text style={styles.toggleButtonText}>
+              Imperial
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={isEnabled ? styles.toggleButton : styles.toggleButtonActive} onPress={toggleSwitch}>
+            <Text style={styles.toggleButtonText}>
+              Metric
+            </Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder={isEnabled ? 'Weight (kg)' : 'Weight (lb)'}
+          keyboardType="numeric"
+          onChangeText={handleWeight}
+          value={weight}
+        />
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder={isEnabled ? 'Height (cm)' : 'Height (in)'}
+          keyboardType="numeric"
+          onChangeText={handleHeight}
+          value={height}
+        />
+        <TouchableOpacity
+          style={styles.submitButton}
+          onPress={() => this.calculate(height, weight)}>
+          <Text style={styles.buttonText}> Calculate BMI </Text>
+        </TouchableOpacity>
+        <Text style={styles.output}>{bmi}</Text>
+        <Text style={styles.resultText}>{bmiLabel}</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    paddingTop: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    paddingTop: 30,
+    paddingBottom: 10,
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: "#33691e"
   },
-  sectionDescription: {
-    marginTop: 8,
+  input: {
+    margin: 15,
+    height: 45,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  submitButton: {
+    backgroundColor: '#689f38',
+    padding: 10,
+    margin: 15,
+    height: 45,
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  output: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  resultText: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#33691e',
+    fontWeight: 'bold',
+  },
+  toggleButton: {
+    backgroundColor: '#e0e0e0',
+    padding: 10,
+    height: 40,
+  },
+  toggleButtonActive: {
+    backgroundColor: '#689f38',
+    padding: 10,
+    height: 40,
+    borderBottomWidth: 5,
+    borderBottomColor: "#33691e"
+  },
+  toggleButtonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 14,
   },
 });
-
-export default App;
